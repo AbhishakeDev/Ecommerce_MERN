c8b4e4c6c97a307539441f6bce2e60efae1af0c1
Abhishek Tripathi <abhishektripathi@Abhisheks-MacBook-Air.local>
2021-04-01 19:49:49 +0530

    data seeding locally to mongodb

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" exact component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
